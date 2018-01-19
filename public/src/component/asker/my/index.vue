4f043a2fb879fb77c6216f40b1497ac3dfe38bbc
Ö캺ͦ <409199338@qq.com>
2018-01-19 10:09:13 +0800

    fenzhi

--- public/src/component/asker/my/index.vue
+++ public/src/component/asker/my/index.vue
@@ -4,24 +4,31 @@
         <v-showLoad v-if="showLoad"></v-showLoad>
         <div class="weui-tab__panel main">
             <div class="main">
-            <div class="top">
+                <div class="top">
                     <img class="img" :src="user.faceUrl">
                     <div class="name">
-                       {{user.nickName}}
+                        {{user.nickName}}
                     </div>
-                <div class="clear"></div>
+                    <div class="clear"></div>
 
-            </div>
-            <router-link to = "../my/income"  class="income"  ><i></i>我的余额
-                <div class="price">￥{{formatPrice(user.balance)}}</div>
-            </router-link>
-            <router-link to = "../my/listen/list" class="listen" ><i></i>我的偷听</router-link>
-            <router-link to = "../my/answer/list" class="answer" ><i></i>我的收听</router-link>
-            <router-link to = "../my/comment/list" class="comment" ><i></i>我的评价</router-link>
+                </div>
+                <router-link to="../my/income" class="income"><i></i>我的现金
+                    <div class="price">￥{{formatPrice(user.balance)}}</div>
+                </router-link>
+
+                <router-link to="../my/income" class="income"><i></i>我的点币
+                    <div class="price">￥{{formatPrice(user.balance)}}</div>
+                </router-link>
+                <router-link to="../my/income" class="income"><i></i>优惠卡券
+                    <div class="price">￥{{formatPrice(user.balance)}}</div>
+                </router-link>
+                <router-link to="../my/listen/list" class="listen"><i></i>我的偷听</router-link>
+                <router-link to="../my/answer/list" class="answer"><i></i>我的收听</router-link>
+                <router-link to="../my/comment/list" class="comment"><i></i>我的评价</router-link>
             </div>
             <div class="join" @click="join()">入驻咨询师</div>
         </div>
-        <v-asker-bottom tabOnIndex="4" ></v-asker-bottom>
+        <v-asker-bottom tabOnIndex="4"></v-asker-bottom>
     </div>
 </template>
 
@@ -120,31 +127,114 @@
 
 
     }
+
 </script>
 <style>
-    .asker_my_index_box .top{ height:  3.176471rem; line-height:  3.176471rem;color:#fff; font-size: 1.058823529411765rem; background:url("../../../images/asker/topBackground.png") no-repeat; padding:0.8823529411764706rem;background-size: 100% 100%;}
-    .asker_my_index_box .top img{ display: block; float:left; width:3.176471rem; height: 3.176471rem; border-radius: 50%;  margin-right: 0.735rem;border: 1px solid #fff}
+    .asker_my_index_box .top {
+        height: 3.176471rem;
+        line-height: 3.176471rem;
+        color: #fff;
+        font-size: 1.058823529411765rem;
+        background: url("../../../images/asker/topBackground.png") no-repeat;
+        padding: 0.8823529411764706rem;
+        background-size: 100% 100%;
+    }
 
-    .asker_my_index_box .main a{ position: relative; height:3.470588235294118rem;    line-height:3.470588235294118rem; background: #fff;
-     color:rgba(36,37,61,1); font-size: 0.8823529411764706rem; padding:  0  0.8823rem;display: block; overflow: hidden; padding-left: 2.66rem; }
-    .asker_my_index_box .main a:nth-child(2){margin-top: 0.41176471rem;}
-    .asker_my_index_box .main a:after{ background: url(../../../images/arrow.png) ; width: 0.9411764705882353rem; height: 0.9411764705882353rem; background-size: 0.9411764705882353rem; position: absolute;
-        right:0.88235rem;top:1.25rem ; content: " "}
-    .asker_my_index_box .main a:before{ content: " "; height: 0.0588235294117647rem; background: rgba(238,238,238,1); display: block; position: absolute; bottom:0;left: 0.9411764705882353rem; width: 100%}
-    .asker_my_index_box .main a .price{position: absolute; right:2.2rem;top:0rem ;color:#FE7301}
-    .asker_my_index_box .main a:last-child:before{
-        display: none;}
+    .asker_my_index_box .top img {
+        display: block;
+        float: left;
+        width: 3.176471rem;
+        height: 3.176471rem;
+        border-radius: 50%;
+        margin-right: 0.735rem;
+        border: 1px solid #fff
+    }
 
+    .asker_my_index_box .main a {
+        position: relative;
+        height: 3.470588235294118rem;
+        line-height: 3.470588235294118rem;
+        background: #fff;
+        color: rgba(36, 37, 61, 1);
+        font-size: 0.8823529411764706rem;
+        padding: 0 0.8823rem;
+        display: block;
+        overflow: hidden;
+        padding-left: 2.66rem;
+    }
 
+    .asker_my_index_box .main a:nth-child(2) {
+        margin-top: 0.41176471rem;
+    }
 
-    .asker_my_index_box .main  a i{ display: block;background: url(../../../images/asker/asker_my_left_icon1.png) no-repeat ; background-size: 1.705882352941176rem; width:1.705882352941176rem; height:1.705882352941176rem;  position: absolute; left: 0.75rem; top:50% ; margin-top: -0.852941176470588rem;}
-    .asker_my_index_box .main  a.income i{ background-position: 0 0 }
-    .asker_my_index_box .main  a.listen i{ background-position: 0 -1.705882352941176rem; }
-    .asker_my_index_box .main  a.answer i{ background-position: 0 -3.411764705882352rem}
-    .asker_my_index_box .main  a.comment i{ background-position: 0 -5.117647058823528rem; }
+    .asker_my_index_box .main a:after {
+        background: url(../../../images/arrow.png);
+        width: 0.9411764705882353rem;
+        height: 0.9411764705882353rem;
+        background-size: 0.9411764705882353rem;
+        position: absolute;
+        right: 0.88235rem;
+        top: 1.25rem;
+        content: " "
+    }
 
+    .asker_my_index_box .main a:before {
+        content: " ";
+        height: 0.0588235294117647rem;
+        background: rgba(238, 238, 238, 1);
+        display: block;
+        position: absolute;
+        bottom: 0;
+        left: 0.9411764705882353rem;
+        width: 100%
+    }
 
+    .asker_my_index_box .main a .price {
+        position: absolute;
+        right: 2.2rem;
+        top: 0rem;
+        color: #FE7301
+    }
 
-   .asker_my_index_box .join{ background: #fff; line-height:3.470588235294118rem; margin: 0.41176471rem 0 ; text-align: center; font-size: 0.8823529411764706rem;color:rgba(36,37,61,1); }
+    .asker_my_index_box .main a:last-child:before {
+        display: none;
+    }
+
+    .asker_my_index_box .main a i {
+        display: block;
+        background: url(../../../images/asker/asker_my_left_icon1.png) no-repeat;
+        background-size: 1.705882352941176rem;
+        width: 1.705882352941176rem;
+        height: 1.705882352941176rem;
+        position: absolute;
+        left: 0.75rem;
+        top: 50%;
+        margin-top: -0.852941176470588rem;
+    }
+
+    .asker_my_index_box .main a.income i {
+        background-position: 0 0
+    }
+
+    .asker_my_index_box .main a.listen i {
+        background-position: 0 -1.705882352941176rem;
+    }
+
+    .asker_my_index_box .main a.answer i {
+        background-position: 0 -3.411764705882352rem
+    }
+
+    .asker_my_index_box .main a.comment i {
+        background-position: 0 -5.117647058823528rem;
+    }
+
+    .asker_my_index_box .join {
+        background: #fff;
+        line-height: 3.470588235294118rem;
+        margin: 0.41176471rem 0;
+        text-align: center;
+        font-size: 0.8823529411764706rem;
+        color: rgba(36, 37, 61, 1);
+    }
 
 </style>
