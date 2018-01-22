d94fcb4eb0cad4793e2a651e722e28cb1ec5e721
ouyangchaoqun <ouyangchaoqun@aliyun.com>
2018-01-22 17:53:07 +0800

    balance

--- public/src/component/asker/my/cash.vue
+++ public/src/component/asker/my/cash.vue
@@ -54,8 +54,8 @@
     .dotcoin_box .banner{ background: #FE7A03 ;height: 9.94rem; overflow:hidden}
     .dotcoin_box .title{ margin-left: 0.94rem ;margin-top: 1.41rem; line-height: 1.24rem;color: #fff;}
     .dotcoin_box .value{font-size: 4rem; margin-top: 0.65rem; margin-left: 0.94rem;line-height: 5.88rem; color: #fff; }
-    .dotcoin_box .btn{ height: 2.94rem; background: #fff; border-bottom: 1px solid #eee;}
+    .dotcoin_box .btn{ height: 2.94rem; background: #fff; border-bottom: 1px solid #eee;position: relative}
     .dotcoin_box .btn .left{ margin-left: 0.88rem;margin-top: 0.94rem; float: left; font-size: 0.88rem; line-height: 1.23rem;color: #4A4A4A;}
-    .dotcoin_box .btn .right{float: right;margin-right:0.9rem; margin-top: 0.94rem; width: 0.8rem; height: 1.13rem; background: url(../../../images/arrow.png) no-repeat;background-size: 100% 80%;  }
+    .dotcoin_box .btn .right{background: url(../../../images/arrow.png) no-repeat;width: 0.9411764705882353rem; height: 0.9411764705882353rem;background-size: 0.9411764705882353rem;position: absolute;right:0.88235rem;top:50%;margin-top:-0.47rem;}
 
 </style>
