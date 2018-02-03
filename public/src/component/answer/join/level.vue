e0b058e9cd85a40d26989ae67c79b97c6dae181b
ouyangchaoqun <ouyangchaoqun@aliyun.com>
2018-02-03 15:31:34 +0800

    测试

--- public/src/component/answer/join/level.vue
+++ public/src/component/answer/join/level.vue
@@ -85,7 +85,7 @@
 
                 },function (json,ix) {
                     _this.showLoad=false;
-                    let fileWidth = parseInt($(body).width()*0.92);
+                    let fileWidth = parseInt($('body').width()*0.92);
                     let fileHeight = parseInt(fileWidth*0.52);
                     _this.certificateFile1 = json.data.path+'?x-oss-process=image/resize,h_'+fileHeight+',w_'+fileWidth+'/quality,q_100';
                 },function (e) {
