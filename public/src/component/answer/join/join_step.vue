316b80c4f85101c41973fbf71b0477dfc4e8187a
ouyangchaoqun <ouyangchaoqun@aliyun.com>
2018-02-07 15:32:09 +0800

    icon

--- public/src/component/answer/join/join_step.vue
+++ public/src/component/answer/join/join_step.vue
@@ -9,7 +9,7 @@
             <img v-if="isModify==1" :src="isShowInfo.faceUrl" alt="">
             <div class="li_right">
                 <div>
-                    <span v-if="faceUrl==''" class="tip_color">请上传头像</span>
+                    <span v-if="faceUrl==''&&isModify==0" class="tip_color">请上传头像</span>
                     <template v-if="faceUrl!=''&&isModify==0">更换头像</template>
                     <template v-if="isShowInfo.faceUrl!=''&&isModify==1">更换头像</template>
                 </div>
@@ -814,7 +814,7 @@
     .join_stepBox .step_detailBox li{height: 2.94rem;line-height:2.94rem;color:rgba(69, 75, 84, 1);border-bottom: 1px solid rgba(224,224,225,1);padding:0 0.88235rem;font-size: 0.8235rem;position: relative;}
     .join_stepBox .step_detailBox li .li_right{float: right;padding-right:1.5rem;width:55%;color: rgba(69, 75, 84, 1) !important;}
     .li_right>div{ width:100%;text-align: right;font-size: 0.8235rem;color: rgba(69, 75, 84, 0.5); overflow: hidden;text-overflow: ellipsis;white-space: nowrap;}
-    .join_stepBox .li_right i{background: url('../../../images/join_right_icon.png') no-repeat center;width: 0.5rem;  height: 1rem;  background-size:100%;  position: absolute;  right: 0.88235rem;  top: 50%;margin-top: -0.47rem;  }
+    .join_stepBox .li_right i{background: url('../../../images/join_right_icon.png') no-repeat center;width: 0.5rem;  height: 1rem;  background-size:100%;  position: absolute;  right: 0.88235rem;  top: 50%;margin-top: -0.5rem;  }
     .joinStep_bottom{padding:1.76471rem 0.88235rem;}
     .join_agre{color:rgba(53, 58, 66, 1);font-size: 0.70588rem;line-height: 1rem;margin-bottom: 1.8rem;}
     .join_agre span{color:rgba(255, 99, 0, 1)}
