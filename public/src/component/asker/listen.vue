92fbd5ecf5dcf8bc86f0e46d7dc59c675c8f30c5
朱重熊 <308083469@qq.com>
2018-03-09 16:12:06 +0800

    时间跳动title

--- public/src/component/asker/listen.vue
+++ public/src/component/asker/listen.vue
@@ -639,7 +639,7 @@
         overflow: hidden;
         text-overflow: ellipsis;
         display: -webkit-box;
-        -webkit-line-clamp: 3;
+        -webkit-line-clamp: 4;
         -webkit-box-orient: vertical;
         margin-bottom: 0.30rem;
     }
