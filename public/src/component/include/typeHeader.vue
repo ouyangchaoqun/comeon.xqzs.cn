19e7553bd49641d24c3d6d129c9440d9b896ad5a
ouyangchaoqun <ouyangchaoqun@aliyun.com>
2018-03-13 14:59:22 +0800

    icon 位置大小测试

--- public/src/component/include/typeHeader.vue
+++ public/src/component/include/typeHeader.vue
@@ -12,20 +12,20 @@
 <style>
     .class_list{  padding:0.16rem 0;background: #fff;}
     .class_list .class_item{  float:left ; width: 20%; text-align: center;color:RGBA(69, 75, 84, 1);font-size: 0.21rem;padding: 6px 0 ; }
-    .addClassImg{height:0.88rem; width:0.88rem; margin:0 auto;margin-bottom: 0.12rem;background: url("http://oss.xqzs.cn/resources/psy/typeHeader_blue_bg.png") no-repeat;background-size: 7rem; border-radius: 50%}
-
-    .class_item:active .addClassImg{background-color: #eee;}
+    .addClassImg{height:0.98rem; width:0.98rem; margin:0 auto;margin-bottom: 0.12rem;background: url("http://oss.xqzs.cn/resources/psy/typeHeader_blue_bg.png") no-repeat ;background-size: 6.9rem; border-radius: 50%}
+    /*.addClassImg{height:0.88rem; width:0.88rem; margin:0 auto;margin-bottom: 0.12rem;background: url("http://oss.xqzs.cn/resources/psy/typeHeader_bg.png") no-repeat;background-size: 6.88rem; border-radius: 50%}*/
+    /*.class_item:active .addClassImg{background-color: #eee;}*/
 
     .class_list .qb{background-position:0.00rem 0.00rem;  }
-    .class_list  .qgkh{background-position:-1.54rem -0rem;  }
-    .class_list .xxl{background-position:-6.1rem -1.82rem; }
-    .class_list .rjgx{background-position:-6.1rem 0.00rem;}
+    .class_list  .qgkh{background-position:-1.5rem -0rem;  }
+    .class_list .xxl{background-position:-5.92rem -1.82rem; }
+    .class_list .rjgx{background-position:-5.92rem 0.00rem;}
     .class_list .zcsy{background-position:-0rem -1.82rem ; }
-    .class_list .hyjt{background-position:-4.6rem -0rem;}
-    .class_list .grcz{background-position:-3.07rem -0rem ;}
-    .class_list .qxgl{background-position:-3.07rem -1.82rem;   }
-    .class_list .xljk{background-position:-1.54rem -1.82rem ;}
-    .class_list .qzjy{background-position:-4.6rem -1.82rem; }
+    .class_list .hyjt{background-position:-4.44rem -0rem;}
+    .class_list .grcz{background-position:-2.98rem -0rem ;}
+    .class_list .qxgl{background-position:-2.98rem -1.82rem;   }
+    .class_list .xljk{background-position:-1.5rem -1.82rem ;}
+    .class_list .qzjy{background-position:-4.44rem -1.82rem; }
 
 
 
