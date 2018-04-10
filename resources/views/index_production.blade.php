b65c3d317c162c64345b41f2fa183c467fd8ee04
朱重熊 <308083469@qq.com>
2018-04-10 11:30:36 +0800

    bug

--- resources/views/index_production.blade.php
+++ resources/views/index_production.blade.php
@@ -3,9 +3,7 @@
 <head>
     <?php  require "static_version.php";  ?>
   <meta charset="utf-8">
-        <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
-        <meta http-equiv="Pragma" content="no-cache" />
-        <meta http-equiv="Expires" content="0" />
+
   <meta name="viewport" content="width=device-width,inital-scale=1.0,maximum-scale=1.0,user-scalable=no;">
       <meta name="x5-orientation" content="portrait">
       <title>心理咨询</title>
