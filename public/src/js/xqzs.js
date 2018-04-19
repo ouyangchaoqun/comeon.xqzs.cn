b35c4287e5092e5d23927a36a26bc02c1a0c1c94
Bozen <771845116@qq.com>
2018-04-19 11:59:07 +0800

    测试

--- public/src/js/xqzs.js
+++ public/src/js/xqzs.js
@@ -918,26 +918,26 @@ var xqzs = {
                     console.log("localIds.length");
                     console.log(localIds.length);
                     for (var i = 0; i < localIds.length; i++) {
-                        console.log("localIds:"+i)
+                        console.log("localIds:" + i)
 
-                            var currLocalId = localIds[i];
-                            wx.getLocalImgData({  // 拿到本地图片
-                                localId: currLocalId, // 图片的localID
-                                success: function (res) {
-                                    console.log("success")
-                                    console.log(res)
-                                    var localData = res.localData; // localData是图片的base64数据，可以用img标签显示
-                                    if (localData.indexOf(";base64,") > 0) {
-                                        localData.replace('jgp', 'jpeg');
-                                    } else {
-                                        localData = "data:image/jpeg;base64," + localData;
-                                    }
-                                    console.log("finish")
-                                    xqzs.oss.uploadPicture($uploadpicinfo, $alioss, {base64: localData}, function (json) {
-                                        finishUploadFun(json,i)
-                                    }, errorFun, i);
+                        var currLocalId = localIds[i];
+                        wx.getLocalImgData({  // 拿到本地图片
+                            localId: currLocalId, // 图片的localID
+                            success: function (res) {
+                                console.log("success")
+                                console.log(res)
+                                var localData = res.localData; // localData是图片的base64数据，可以用img标签显示
+                                if (localData.indexOf(";base64,") > 0) {
+                                    localData.replace('jgp', 'jpeg');
+                                } else {
+                                    localData = "data:image/jpeg;base64," + localData;
                                 }
-                            });
+                                console.log("finish")
+                                xqzs.oss.uploadPicture($uploadpicinfo, $alioss, {base64: localData}, function (json,ix) {
+                                    finishUploadFun(json,ix)
+                                }, errorFun, i);
+                            }
+                        });
 
                     }
                 }
