fcc2615511fecdf09964847b91a20af20e1e24bc
ouyangchaoqun <ouyangchaoqun@aliyun.com>
2018-04-19 16:26:11 +0800

    测试

--- public/src/component/answer/join/voice.vue
+++ public/src/component/answer/join/voice.vue
@@ -151,6 +151,7 @@
             //上传合成音频
             subComposeBgm:function () {
                 let _this = this;
+                console.log(_this.localId)
                 //发送合成音音频到微信服务器并获取serverId
                 xqzs.wx.voice.upload(_this.localId,function (serverId) {
                     _this.serviceId = serverId;
